46dc1909afdcf7441d275fe4b21cef1ae5d3430f
SomethingOnu <rogatelice@protonmail.com>
2021-02-06 12:49:10 +0900

    ソースを公開

---
+++ index.js
@@ -0,0 +1,22 @@
+const gallery = document.getElementById('gallery');
+
+for (let i = 1; i <= 534; i++) {
+    const number = i.toString().padStart(4, '0');
+
+    const photo = document.createElement('a');
+    photo.className        = 'photo';
+    photo.href             = `photo/${number}.jpg`;
+    photo.dataset.lightbox = "photo";
+
+    const img = document.createElement('img');
+    img.className   = 'lazyload';
+    img.dataset.src = `photo/${number}.jpg`;
+    img.src         = `photo-S/${number}.jpg`;
+    img.title       = '写真';
+    img.alt         = 'Photo by Something Onu';
+
+    photo.appendChild(img);
+    gallery.appendChild(photo);
+};
+
+window.onload = () => lazyload();
